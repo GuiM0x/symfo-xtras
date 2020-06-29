3dbe748a6b986dcf0e5e12818b8ed7d3b798ddca
GuiM0x <martox.lintox@gmail.com>
2020-06-29 22:12:43 +0200

    XFileUploader updated

--- src/XFileUploader.php
+++ src/XFileUploader.php
@@ -2,10 +2,52 @@
 
 namespace Guiman\SymfoXtras;
 
+// require_once __DIR__.'/../vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\String\Slugger\SluggerInterface;
+// use Symfony\Component\DependencyInjection\ContainerBuilder;
+
 class XFileUploader
 {
-    public static function sayHello(): string
+    private string $targetDirectory;
+    private SluggerInterface $slugger;
+    // private ContainerBuilder $containerBuilder;
+
+    public function __construct(SluggerInterface $slugger)
     {
-        return "Hello world";
+        $targetDirectory = null;
+        $this->slugger = $slugger;
+    }
+
+    public function upload(UploadedFile $file): string
+    {
+        assert(
+            $this->targetDirectory,
+            "A target directory is needed before upload a file. \nUse XFileUploader::setTargetDirectory to set it."
+        );
+
+        $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+        $safeFilename = $this->slugger->slug($originalFilename);
+        $fileName = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
+
+        try {
+            $file->move($this->getTargetDirectory(), $fileName);
+        } catch (FileException $e) {
+            // ... handle exception if something happens during file upload
+        }
+
+        return $fileName;
+    }
+
+    public function setTargetDirectory(string $targetDirectory): void
+    {
+        $this->targetDirectory = $targetDirectory;
+    }
+
+    public function getTargetDirectory(): string
+    {
+        return $this->targetDirectory;
     }
 }
