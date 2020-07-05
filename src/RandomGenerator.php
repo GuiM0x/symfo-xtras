9593fb09de1308b24fd3c9eb2bae2a9b696fd31e
GuiM0x <martox.lintox@gmail.com>
2020-07-05 11:53:36 +0200

    Random generator added + comments for classes

---
+++ src/RandomGenerator.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+ * (c) Guillaume Manier <guillaume.manier.pro@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Guiman\SymfoXtras;
+
+class RandomGenerator
+{
+    public function __construct()
+    {
+        
+    }
+
+    /**
+     * Return a random number between min and max.
+     * Don't use it if you need strong random, use impartialRand function instead.
+     *
+     * @param int $min - Value min that can be return
+     * @param int $max - Value max that can be return
+     * @return int
+     */
+    public function pseudoRand(int $min, int $max): int
+    {
+        mt_srand($this->generateSeed());
+        return mt_rand($min, $max);
+    }
+
+    /**
+     * Return a strong random number between min and max.
+     *
+     * @param int $min - Value min that can be return
+     * @param int $max - Value max that can be return
+     * @return int
+     */
+    public function impartialRand(int $min, int $max): int
+    {
+        return random_int($min, $max);
+    }
+
+    /**
+     * Returns a strong random sequence of characters.
+     *
+     * @param int $length - Represents the length of the returned key
+     * @return string
+     */
+    public function generateKey(int $length = 32): string
+    {
+        $key = "";
+        $cstrong = null;
+        
+        while(!$cstrong){
+            $bytes = openssl_random_pseudo_bytes($length/2, $cstrong);
+            $key = bin2hex($bytes);
+        }
+
+        return $key;
+    }
+
+    /**
+     * Generates a seed based on time.
+     * The seed is used with mt_rand generator.
+     *
+     * @return float
+     */
+    private function generateSeed(): float
+    {
+        list($usec, $sec) = explode(' ', microtime());
+        $usec *= 1000000;
+        return ($usec + $sec);
+    }
+}
--- src/XFileUploader.php
+++ src/XFileUploader.php
@@ -1,53 +1,49 @@
 <?php
 
-namespace Guiman\SymfoXtras;
+/*
+ * (c) Guillaume Manier <guillaume.manier.pro@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
-// require_once __DIR__.'/../vendor/autoload.php';
+namespace Guiman\SymfoXtras;
 
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\String\Slugger\SluggerInterface;
-// use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class XFileUploader
 {
-    private string $targetDirectory;
     private SluggerInterface $slugger;
-    // private ContainerBuilder $containerBuilder;
 
+    /**
+     * The slugger is needed in upload function to transform a file name to a safe file name.
+     */
     public function __construct(SluggerInterface $slugger)
     {
-        $this->targetDirectory = "";
         $this->slugger = $slugger;
     }
 
-    public function upload(UploadedFile $file): string
+    /**
+     * Uploads a file to target directory and returns the new file's name.
+     *
+     * @param UploadedFile $file      - Symfony's object returned by an input of type file
+     * @param string $targetDirectory - Path that represents the target directory where upload the file
+     * @return string
+     */
+    public function upload(UploadedFile $file, string $targetDirectory): string
     {
-        assert(
-            $this->targetDirectory !== "",
-            "A target directory is needed before upload a file. \nUse XFileUploader::setTargetDirectory to set it."
-        );
-
         $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
         $safeFilename = $this->slugger->slug($originalFilename);
         $fileName = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
 
         try {
-            $file->move($this->getTargetDirectory(), $fileName);
+            $file->move($targetDirectory, $fileName);
         } catch (FileException $e) {
-            // ... handle exception if something happens during file upload
+            throw $e;
         }
 
         return $fileName;
-    }
-
-    public function setTargetDirectory(string $targetDirectory): void
-    {
-        $this->targetDirectory = $targetDirectory;
-    }
-
-    public function getTargetDirectory(): string
-    {
-        return $this->targetDirectory;
     }
 }
